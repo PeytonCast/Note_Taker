3bf362660887318dddbab7dc32fa6d5a12fe955d
Peyton C <peyton.castill0@icloud.com>
2022-09-12 16:29:00 -0400

    started writing an api to post and receive message

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,32 @@
+// GIVEN a note-taking application
+// WHEN I open the Note Taker
+// get * might be needed 
+// THEN I am presented with a landing page with a link to a notes page
+
+// WHEN I click on the link to the notes page
+//i need to validate that link
+
+// THEN I am presented with a page with existing notes listed in the left-hand column,
+// plus empty fields to enter a new note title and the note’s text in the right-hand column
+// i will need to build an api and be able to post and get json to the page 
+
+// WHEN I enter a new note title and the note’s text
+// probly going to need to use a hide method and an if else statement to check if there needs to be content in text area
+// THEN a Save icon appears in the navigation at the top of the page
+
+// WHEN I click on the Save icon
+// this save icon will need to be linked to a post request
+// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
+
+// WHEN I click on an existing note in the list in the left-hand column
+// this would be a get request for that specific json
+
+// THEN that note appears in the right-hand column
+// WHEN I click on the Write icon in the navigation at the top of the page
+
+// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
+// this is some type of editing function to rewite a json i will need to use a PUT method
+
 //this is where we will be validating the html and server code
 
 // The following HTML routes should be created:
---
+++ assets/js/server.js
@@ -0,0 +1,53 @@
+const fs = require("fs/promises")
+
+//makes api 
+const express = require("express")
+
+//makes unique ID
+const { v4: uuid } = require("uuid")
+//const { response } = require("express")
+
+//rename express
+const app = express()
+//middle ware
+app.use(express.json())
+
+
+//gets a comment by id
+app.get("/comments/:id", async (req, res) => {
+    const id = req.params.id
+    let content;
+
+    try {
+
+        //TODO rewrite this to read from data base json
+        content = await fs.readFile(`data/comments/${id}.txt`, "utf-8")
+    } catch (err) {
+        return res.sendStatus(404)
+    }
+    res.json({
+        content: content
+    })
+
+})
+
+//posts a new comment
+app.post("/comments", async (req, res) => {
+    const id = uuid()
+    const content = req.body.content
+    
+    if (!content){
+        return res.sendStatus(400)
+    }
+    //TODO rewrite this to push or append to data base json
+    await fs.mkdir("data/comments", {recursive: true})
+    await fs.writeFile(`data/comments/${id}.txt`, content)
+    res.status(201).json({
+        id: id
+    })
+
+})
+
+
+app.listen(3000, () => console.log("api server running"))
+

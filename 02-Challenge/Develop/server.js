685270307ddef5c97b6c6f79788a435beca23394
Peyton C <peyton.castill0@icloud.com>
2022-09-15 16:48:24 -0400

    I finaly got the delete function to work.

--- 02-Challenge/Develop/server.js
+++ 02-Challenge/Develop/server.js
@@ -1,5 +1,5 @@
 const fs = require("fs/promises")
-
+let db = require("./db/db.json")
 //makes api 
 const express = require("express")
 
@@ -15,46 +15,48 @@ app.use(express.json())
 
 //fix this
 //gets a comment by id
-app.get("/api/notes/:id", async (req, res) => {
-    const id = req.params.id
-    let content;
-
-    try {
-        //TODO rewrite this to read from data base json
-        content = await fs.readFile('db.json')
-    } catch (err) {
-        return res.sendStatus(404)
-    }
-    res.json({
-        content: content
-    })
-
+app.get("/api/notes", async(req, res) => {
+    
+    const info = await fs.readFile('./db/db.json', 'utf8')
+    res.json(JSON.parse(info))
+   
 })
+app.delete("/api/notes/:id", function (req, res) {
 
+    fs.readFile(`./db/db.json`, "utf8").then((data) => {
+        const oldData = JSON.parse(data)
+        const { id } = req.params
+        let index = oldData.findIndex((note) => note.id === id)
+        if (index >= 0)  { //if id doesnt exist then send status for 404
+            let deletedNote = oldData.splice(index, 1) //removes the object
+            console.log('deleted note',deletedNote)
+            console.log("----------------------")
+            console.log('data base',oldData)
+            fs.writeFile(`./db/db.json`, JSON.stringify(oldData, null, "  "))
+            res.send(deletedNote)}//sends the response of what was deleted
+            else res.status(404).send("not found")
+    })
+    
+}
+)
 //posts a new note
 app.post("/api/notes", async (req, res) => {
     //using the destructing method
     const { title, text } = req.body;
-    //if both the title and text are present then write the object
+    //if both the title and text are present then write the object 
     if (title && text){
         const newNote = {
             title,
             text,
             id: uuid()
         };
-        // const noteString = JSON.stringify(newNote);
-
-        fs.readFile(`./db/db.json`, "utf8"
-           
-            // err
-            //   ? console.error(err)
-            //   : console.log(`${newNote.title} has been written to JSON file`)
-          ).then((data) => {
+        
+        fs.readFile(`./db/db.json`, "utf8").then((data) => {
 
             console.log(data)
             const oldData = JSON.parse(data)
             oldData.push(newNote)
-            fs.writeFile(`./db/db.json`, JSON.stringify(oldData) )
+            fs.writeFile(`./db/db.json`, JSON.stringify(oldData, null, "  ") )
             const response = {
                 status: 'success',
                 body: newNote,
